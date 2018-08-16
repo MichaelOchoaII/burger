47b5ff1d6302123a4df13691cdaaa35541652385
Michael Ochoa <jdmmike@me.com>
2018-08-16 10:50:43 -0700

    delete

--- db/seeds.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Western Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Original Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Origianal Double Burger', false);
-
